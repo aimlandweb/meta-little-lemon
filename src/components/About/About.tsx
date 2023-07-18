6117b836f3890073a9e4750fb008069705efa456
prodev2023 <rinil.kunhiraman@protonmail.com>
2023-07-18 12:07:50 -0700

    About Component
     - About Component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,10 +6,18 @@ const About = () => {
 			<div className={styles.aboutInfo}>
 				<h1 className={styles.restaurantName}>Little Lemon</h1>
 				<h3 className={styles.location}>Chicago</h3>
-				<p className={styles.description}>Lorem Ipsum ....</p>
+				<p className={styles.description}>
+					Welcome to Little Lemon, Chicago's charming neighborhood bistro, a
+					dream brought to life by two friends, Arian and Mario! Indulge in
+					simple food and classic cocktails in our lively yet casual
+					environment. Our locally sourced menu, complemented by daily specials,
+					brings the flavors of our community to your table. Join Arian and
+					Mario for a delightful dining experience filled with warmth, laughter,
+					and the zest for life!
+				</p>
 			</div>
 			<div className={styles.image}>
-				<img src='' alt='Restaurant Image' />
+				<img src='/owners.jpg' alt='Restaurant Image' />
 			</div>
 		</section>
 	);
